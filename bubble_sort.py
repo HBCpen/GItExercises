715e4d7a432333840cc4bc128b6aa6fe90c77751
tora2rou <corocorokenta0706@gmail.com>
2025-06-17 17:09:38 +0900

    test

--- bubble_sort.py
+++ bubble_sort.py
@@ -9,8 +9,8 @@ def bubble_sort(arr):
 if __name__ == "__main__":
     # MODIFICATION_POINT_FOR_CONFLICT_EXERCISE
     data = [64, 34, 25, 12, 22, 11, 90]
+    data_2 = [74,78,82,84,92.5,93,100]
     print("Original array:", data)
     sorted_data = bubble_sort(data)
     print("Sorted array:", sorted_data)
-
 

06d20ec2e0a9c548ff08562d180874f46fdd75c9
HBCpen <139545654+HBCpen@users.noreply.github.com>
2025-06-24 00:29:10 +0900

    this is test msg, you dont have to care about this.

--- bubble_sort.py
+++ bubble_sort.py
@@ -9,7 +9,7 @@ def bubble_sort(arr):
 
 if __name__ == "__main__":
     # MODIFICATION_POINT_FOR_CONFLICT_EXERCISE
-    data1 = [99, 88, 45, 33, 22, 11, 43]
+    data1 = [64, 34, 25, 12, 22, 11, 90]
     print("Original array:", data1)
     sorted_data = bubble_sort(data1)
     print("Sorted array:", sorted_data)
